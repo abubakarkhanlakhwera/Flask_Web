1551237494752cf1e81f27841b901f3c1277d1ef
Abubakar khan Lakhwera <109948432+abubakarkhanlakhwera@users.noreply.github.com>
2024-11-11 23:10:20 +0500

    juicy

--- introduction_to_flask/app.py
+++ introduction_to_flask/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route('/<route>')
 def home(route):
-    return  f'<h1>Welcome to the {route.title()} page!</h1>'
+    return f'<h1>Welcome to the {route.title()} page!</h1>'
+
+@app.route('/addition/<int:num1>/<int:num2>')
+def addition(num1, num2):
+    return f'<h1>Input was {num1} and {num2} and the sum is {num1 + num2}</h1>'
 
 @app.route('/about')
 def about():
